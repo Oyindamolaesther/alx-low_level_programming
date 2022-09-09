8fd023b98b28f97133e98010308d9c82cca633ed
Oyindamolaesther <oyindamolaesther231@gmail.com>
2022-09-08 18:20:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+       return (0);
+}       
