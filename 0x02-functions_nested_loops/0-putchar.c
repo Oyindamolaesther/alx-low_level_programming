1c5161b45c64c2288118e6556f3fe7abae52195c
Oyindamolaesther <oyindamolaesther231@gmail.com>
2022-09-13 18:00:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Description: 'Printing characters using while statement'
- * Return: Always 0 (Success)
- */
 
+/**
+ * main - print putchar
+ * Return: always 0
+ */
 
 int main(void)
 {
-	char *p = "_putchar";
-	int i;
-
-	i = 0;
-	while (p[i] != '\0')
-	{
-		_putchar(word[i]);
-		i++;
-
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
